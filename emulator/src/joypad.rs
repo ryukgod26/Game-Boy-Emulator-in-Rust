dd7665f5e65295801c3bcc947719893f5ac2bd0e
raviwhitehat <ekravi26@gmail.com>
2026-05-09 23:13:12 +0530

    Add to_byte method to Joypad struct and implement bit utility function

--- emulator/src/joypad.rs
+++ emulator/src/joypad.rs
@@ -35,4 +35,19 @@ impl Joypad{
             right: false,
         }
     }
+
+    pub fn to_byte(&self) -> u8{
+        let column_bit = if self.column == Column::Zero{
+            1 << 5
+        }else{
+            1<<4
+        };
+        let bit_4 = bit(!(((self.down && self.reading_column_0()) || (self.start && self.reading_column_1())))) << 3;
+        let bit_3 = bit(!((self.up && self.reading_column_0()) || (self.select && self.reading_column_1()))) << 2;
+        let bit_2 = bit(!((self.left && self.reading_column_0()) || (self.b && self.reading_column_1()))) << 1;
+        let bit_1 = bit(!((self.right && self.reading_column_0()) || (self.a && self.reading_column_1())));
+
+        let row_bits = bit_4 | bit_3 | bit_2 | bit_1;
+        column_bit | row_bits
+    }
 }
