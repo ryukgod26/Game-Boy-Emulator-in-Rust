23b845e936cfa4bab63239e680ae8e6ab02b0a85
root <root@localhost.localdomain>
2026-05-09 14:56:02 +0000

    Added memory constants in memory_bus

--- emulator/src/memory_bus.rs
+++ emulator/src/memory_bus.rs
@@ -1,5 +1,51 @@
 use super::GPU;
 
+pub const BOOT_ROM_START: usize = 0x00;
+pub const BOOT_ROM_END: usize = 0xFF;
+pub const BOOT_ROM_SIZEi: usize = BOOT_ROM_END - BOOT_ROM_START + 1;
+
+pub const ROM_BANK_0_START: usize = 0x0000;
+pub const ROM_BANK_0_END: usize = 0x3FFF;
+pub const ROM_BANK_0_SIZE: usize = ROM_BANK_0_END - ROM_BANK_0_START + 1;
+
+pub const ROM_BANK_N_START: usize = 0x4000;
+pub const ROM_BANK_N_END: usize = 0x7FFF;
+pub const ROM_BANK_N_SIZE: usize = ROM_BANK_N_END - ROM_BANK_N_START + 1;
+
+pub const EXTERNAL_RAM_START: usize = 0xA000;
+pub const EXTERNAL_RAM_END: usize = 0xBFFF;
+pub const EXTERNAL_RAM_SIZE: usize = EXTERNAL_RAM_END - EXTERNAL_RAM_START + 1;
+
+pub const WORKING_RAM_START: usize = 0xC000;
+pub const WORKING_RAM_END: usize = 0xDFFF;
+pub const WORKING_RAM_SIZE: usize = WORKING_RAM_END - WORKING_RAM_START + 1;
+
+pub const ECHO_RAM_START: usize = 0xE000;
+pub const ECHO_RAM_END: usize = 0xFDFF;
+pub const ECHO_RAM_SIZE: usize = ECHO_RAM_END - ECHO_RAM_START + 1;
+
+pub const OAM_START: usize = 0xFE00;
+pub const OAM_END: usize = 0xFE9F;
+pub const OAM_SIZE: usize = OAM_END - OAM_START + 1;
+
+pub const UNUSED_START: usize = 0xFEA0;
+pub const UNUSED_END: usize = 0xFEFF;
+pub const UNUSED_SIZE: usize = UNUSED_END - UNUSED_START + 1;
+
+pub const IO_REGISTERS_START: usize = 0xFF00;
+pub const IO_REGISTERS_END: usize = 0xFF7F;
+pub const IO_REGISTERS_SIZE: usize = IO_REGISTERS_END - IO_REGISTERS_START + 1;
+
+pub const ZERO_PAGE_START: usize = 0xFF80;
+pub const ZERO_PAGE_END: usize = 0xFFFE;
+pub const ZERO_PAGE_SIZE: usize = ZERO_PAGE_END - ZERO_PAGE_START + 1;
+
+pub const INTERRUPT_ENABLE_REGISTER: usize = 0xFFFF;
+
+pub const VBLANK_VECTOR: u16 = 0x40;
+pub const LCDSTAT_VECTOR: u16 = 0x48;
+pub const TIMER_VECTOR: u16 = 0x50;
+
 pub const VRAM_BEGIN: usize = 0x8000;
 pub const VRAM_END: usize = 0x9FFF;
 pub const VRAM_SIZE: usize = VRAM_END - VRAM_BEGIN + 1;
