138bbc0515e338c871debcd169908b13ad90f019
raviwhitehat <ekravi26@gmail.com>
2026-05-13 22:13:33 +0530

    Added buffer_from file function and constants in main.rs

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -8,6 +8,8 @@ mod joypad;
 mod timer;
 mod utils;
 mod interrupt_flags;
+use std::io::Read;
+
 pub use cpu::CPU;
 pub use instruction::*;
 pub use memory_bus::*;
@@ -17,9 +19,19 @@ pub use gpu::GPU;
 
 const ENLARGEMENT_FACTOR: usize = 1;
 const WINDOW_DIMENSIONS: [usize; 2] = [(160 * ENLARGEMENT_FACTOR), (144 * ENLARGEMENT_FACTOR)];
-
+const ONE_SECOND_IN_MICROS: usize = 1000000000;
+const ONE_SECOND_IN_CYCLES: usize = 4190000;
+const ONE_FRAME_IN_CYCLES: usize = 70224;
+const NUMBER_OF_PIXELS: usize = 23040;
 
 
 fn main() {
     println!("Hello, world!");
+}
+
+fn buffer_from_file(path: &str) -> Vec<u8>{
+    let mut file = std::fs::File::open(path).expect("File not Found");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Could not read file");
+    buffer
 }
